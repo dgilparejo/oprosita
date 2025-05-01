ccc7616d0aa26b52c28f3bbed6762fb13852966a
David <d1v3dgp@gmail.com>
2025-05-01 12:34:29 +0200

    feat: commit initial 0.0.1

---
+++ backend/app/src/test/java/com/oprosita/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oprosita.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
