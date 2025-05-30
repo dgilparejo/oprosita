4623d6445af919b1b8d2737f50f8163aa505c1d8
lNuclear8 <151663815+lNuclear8@users.noreply.github.com>
2025-05-30 20:24:48 +0200

    feat: funciona get grupos

--- frontend/src/app/pages/groups/groups.component.ts
+++ frontend/src/app/pages/groups/groups.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ListComponent } from '../../components/list/list.component';
 import { ActivatedRoute, Router } from '@angular/router';
+import { GruposService } from '../../api/api/grupos.service';
+import { Grupo } from '../../api/model/grupo';
 
 @Component({
   selector: 'app-groups',
@@ -11,15 +13,25 @@ import { ActivatedRoute, Router } from '@angular/router';
   styleUrls: ['./groups.component.css']
 })
 export class GroupsComponent implements OnInit {
-  grupos = ['Grupo 1', 'Grupo 2', 'Grupo 3'];
+  grupos: Grupo[] = [];
   origen: 'grupos' | 'alumnado' = 'grupos';
+
+  get nombresGrupos(): string[] {
+    return this.grupos.map(g => g.nombre);
+  }
 
   private router = inject(Router);
   private route = inject(ActivatedRoute);
+  private gruposService = inject(GruposService);
 
   ngOnInit(): void {
     const currentPath = this.router.url.split('/')[1];
     this.origen = currentPath === 'alumnado' ? 'alumnado' : 'grupos';
+
+    this.gruposService.getGrupos().subscribe({
+      next: (res) => this.grupos = res,
+      error: (err) => console.error('Error al obtener grupos:', err)
+    });
   }
 
   irAMeses(nombreGrupo: string) {
